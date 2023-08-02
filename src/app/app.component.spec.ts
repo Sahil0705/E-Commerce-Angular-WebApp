d3269f0f393678a4d2aa25e4052b41966460487d
Sahil Donde <sahildonde@policybazaar.com>
2023-08-02 11:10:16 +0530

    E-Commerce Angular Web App - 1st commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'E-Commerce-Angular-WebApp'`, () => {
+  it(`should have as title 'ecomm-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('E-Commerce-Angular-WebApp');
+    expect(app.title).toEqual('ecomm-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('E-Commerce-Angular-WebApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ecomm-project app is running!');
   });
 });
